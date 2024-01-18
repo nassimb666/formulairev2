ebb5139dcdf244c0c84f4809ec28302f4c5d11b8
nassimb666 <bnassim750@gmail.com>
2024-01-18 10:14:25 +0100

    up

--- views/view_signin.php
+++ views/view_signin.php
@@ -1,42 +1,7 @@
 <?php
-session_start();
+ require_once"../controllers/controler_signin.php";
 
-require_once "../config.php";
-require_once "../models/utilisateur.php";
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = isset($_POST["mail"]) ? trim($_POST["mail"]) : null;
-    $password = isset($_POST["password"]) ? trim($_POST["password"]) : null;
-
-    if (empty($email) || empty($password)) {
-        echo "Veuillez remplir tous les champs du formulaire.";
-        exit();
-    }
-
-    try {
-        $db = Database::getInstance();
-        $query = $db->prepare("SELECT * FROM `userprofil` WHERE `user_email` = :email LIMIT 1");
-        $query->bindValue(':email', $email, PDO::PARAM_STR);
-        $query->execute();
-        $user = $query->fetch(PDO::FETCH_ASSOC);
-
-        if ($user) {
-            if (password_verify($password, $user['user_password'])) {
-                $_SESSION['user'] = $user;
-                header("Location: dashboard.php");
-                exit();
-            } else {
-                echo "Mot de passe incorrect.";
-            }
-        } else {
-            echo "Aucun compte trouvé avec cette adresse e-mail.";
-        }
-    } catch (PDOException $e) {
-        echo 'Erreur : ' . $e->getMessage();
-    }
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -49,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <body>
     <h1>Veuillez vous identifier</h1>
 
-    <form action="login.php" method="POST" novalidate>
+    <form action="" method="POST" novalidate>
         <label for="mail">Identifiant:</label>
         <input type="text" name="mail" id="mail" placeholder="exemple@mail.fr" required>
 
